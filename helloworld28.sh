74dd967bfe9e84b2a9c67c6a3eed66f00a0a74cb
Gayathri <nagagayathrigongati@gmail.com>
2025-06-20 11:39:25 +0530

    updating the code

--- helloworld28.sh
+++ helloworld28.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "hello this is gayathri"
+
+ echo "Gayathri: hi harsha"
+ echo "Harsha: Hi Gayathri"
+ echo "Gayathri: how are you harsha"
+ echo "Harsha: i am good how are you"
+
