2e466864c94a650e580d6cd8343dd9265d22b74c
Gayathri <nagagayathrigongati@gmail.com>
2025-06-21 10:45:33 +0530

    username and password script

---
+++ variables4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username: "
+
+read USERNAME
+
+echo "entered username: $USERNAME"
+
+echo "please enter your password: "
+
+read -s PASSWORD
+
+echo "entered username: $PASSWORD"
