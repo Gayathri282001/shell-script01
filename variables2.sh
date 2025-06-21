873653e79e96ea418d1b955ab2f34d25bbe61a79
Gayathri <nagagayathrigongati@gmail.com>
2025-06-21 10:22:30 +0530

    command line variables

---
+++ variables2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:Hi This is $PERSON1"
+echo "$PERSON2:Hi This is $PERSON2"
+echo "$PERSON1:how are you"
+echo "$PERSON2:i am fine, how are you"
+echo "$PERSON1:i am good"
